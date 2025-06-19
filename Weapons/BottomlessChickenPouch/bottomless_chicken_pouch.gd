6a6f9d9edb409f0af05656fa5ae6619e701f79f7
Gee <alickoamarko@gmail.com>
2025-06-19 15:44:31 +0200

    Added weapon: Bottomless Chicken Pouch

---
+++ Weapons/BottomlessChickenPouch/bottomless_chicken_pouch.gd
@@ -0,0 +1,34 @@
+extends Weapon
+
+
+const DEFAULT_ATTACK_DAMAGE = 20
+const PROJECTILE = preload("BottomlessChickenPouchProjectile.tscn")
+
+
+func _ready() -> void:
+	attack_damage = DEFAULT_ATTACK_DAMAGE
+
+func attack(attack_direction: Vector2):
+	if $AttackCooldown.is_stopped():
+		var projectile_vector: Vector2 = (
+			holder.linear_velocity.project(attack_direction) *
+			remap(
+				holder.linear_velocity.normalized().dot(attack_direction),
+				-1.0,
+				1.0,
+				0.0,
+				1.0
+			)
+		)
+		
+		get_parent().add_child(
+			PROJECTILE.instantiate()
+			.set_damage(calculate_attack_damage())
+			.set_start_pos(global_position)
+			.add_collision_exception(holder)
+			.add_collision_exception(self)
+			.shoot(projectile_vector)
+		)
+		
+		$AttackCooldown.start()
+		super(attack_direction)
---
+++ Weapons/BottomlessChickenPouch/bottomless_chicken_pouch_projectile.gd
@@ -0,0 +1,50 @@
+extends Entity
+
+
+const DEFAULT_DAMAGE = 20
+
+
+var damage: int = DEFAULT_DAMAGE
+
+
+func set_damage(damage: int):
+	self.damage = damage
+	return self
+
+func set_start_pos(start_pos: Vector2):
+	global_position = start_pos
+	return self
+
+func add_collision_exception(target: Node2D):
+	add_collision_exception_with(target)
+	return self
+
+func shoot(attack_vector: Vector2):
+	apply_central_impulse(attack_vector)
+	return self
+
+
+func _on_life_timer_timeout() -> void:
+	queue_free()
+
+func _ready() -> void:
+	await get_tree().physics_frame
+	rotation = linear_velocity.angle()
+	angular_velocity = sqrt(linear_velocity.length())
+
+func _physics_process(delta: float) -> void:
+	pass # Override & stop sprite flip behavior
+		# (for rotation)
+
+func _process(delta: float) -> void:
+	$Sprite2D.modulate.a = remap(
+		$LifeTimer.time_left,
+		$LifeTimer.wait_time,
+		0,
+		1.0,
+		0.0
+	)
+
+func _on_enemy_collider_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		body.hit(damage)
