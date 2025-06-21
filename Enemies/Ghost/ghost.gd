d7ce117a50b16de8a3e2e2eceb581b90cdcd7683
Gee <alickoamarko@gmail.com>
2025-06-21 21:24:38 +0200

    Added Enemy: Ghost

---
+++ Enemies/Ghost/ghost.gd
@@ -0,0 +1,15 @@
+extends Enemy
+
+
+const DEFAULT_MOVE_SPEED = 100
+const DEFAULT_HEALTH = 200
+
+
+func _ready() -> void:
+	move_speed = DEFAULT_MOVE_SPEED
+	health = DEFAULT_HEALTH
+
+func move():
+	apply_central_force(
+		(target.position - position).normalized().rotated(PI/4) * move_speed
+	)
