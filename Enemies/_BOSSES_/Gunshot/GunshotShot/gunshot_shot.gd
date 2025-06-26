67271c84d9abed6407d7e14ae98f6354b1a8a4e8
Gee <alickoamarko@gmail.com>
2025-06-26 18:33:35 +0200

    WIP Gunshot

--- Enemies/_BOSSES_/Gunshot/GunshotShot/gunshot_shot.gd
+++ Enemies/_BOSSES_/Gunshot/GunshotShot/gunshot_shot.gd
@@ -1,4 +1,33 @@
 extends Area2D
 
+# Set collision mask to target
 
-# TODO
+
+@export var shot_color := Color(0, 0, 0, 1)
+
+
+func set_length(length: int):
+	var new_shape := RectangleShape2D.new()
+	new_shape.size = Vector2(length, 1)
+	$CollisionShape2D.shape = new_shape
+	$CollisionShape2D.position.x = length/2
+	$Line2D.points[1].x = length
+	return self
+
+func _process(delta: float) -> void:
+	$Line2D.modulate.a = remap(
+		$LifeTimer.time_left,
+		$LifeTimer.wait_time,
+		0,
+		1.0,
+		0.0
+	)
+
+func _on_aim_timer_timeout() -> void:
+	$Line2D.default_color = shot_color
+	for body in get_overlapping_bodies():
+		body.hit.emit()
+	$LifeTimer.start()
+
+func _on_life_timer_timeout() -> void:
+	queue_free()
--- Enemies/_BOSSES_/Gunshot/gunshot.gd
+++ Enemies/_BOSSES_/Gunshot/gunshot.gd
@@ -3,7 +3,15 @@ extends Enemy
 
 const DEFAULT_HEALTH = 10000
 const SHOT = preload("GunshotShot/GunshotShot.tscn")
+const SHOT_SPREAD = PI/2
+const SHOT_COUNT = 11
+const MAX_SHOT_LENGTH = 180
+const SHOT_OFFSET = 16
+const WALL_MASK = 0b00000000_00000000_00100000
 @export var drop = preload("res://Weapons/ShootyThingamabob/ShootyThingamabob.tscn")
+
+
+var shooting: bool = false
 
 
 func _ready() -> void:
@@ -11,14 +19,56 @@ func _ready() -> void:
 	health = DEFAULT_HEALTH
 	max_health = health
 
+func _on_attack_timer_timeout() -> void:
+	shooting = true
+
 func move():
-	global_rotation = (
+	var target_vector := (
 		target.global_position - global_position
-	).angle()
-	
-	apply_central_force(
-		(target.global_position - global_position).normalized().rotated(PI/3) *
-		move_speed
 	)
 	
-	# TODO: Shoot GunshotShot's
+	if not shooting:
+		global_rotation = target_vector.angle()
+		apply_central_force(
+			target_vector.normalized().rotated(PI/3) *
+			move_speed
+		)
+	
+	else:
+		# Create & rotate shots
+		var shots: Array[Area2D] = []
+		for shot_index in range(SHOT_COUNT):
+			var shot: Area2D = SHOT.instantiate()
+			shot.global_position = (
+				global_position +
+				target_vector.normalized() * SHOT_OFFSET
+			)
+			shot.rotation = (
+				target_vector.angle() +
+				-SHOT_SPREAD/2 + (shot_index * SHOT_SPREAD/SHOT_COUNT)
+			)
+			get_parent().add_child(shot)
+			shots.append(shot)
+		
+		# Clip shot length to wall (if hit)
+		for shot in shots:
+			var raycast_result = get_world_2d().direct_space_state.intersect_ray(
+				PhysicsRayQueryParameters2D.create(
+					shot.global_position,
+					(
+						shot.global_position +
+						Vector2.from_angle(shot.rotation) *
+						MAX_SHOT_LENGTH
+					),
+					WALL_MASK
+				)
+			)
+			if raycast_result:
+				shot.set_length(
+					(
+						raycast_result.position -
+						shot.global_position
+					).length()
+				)
+		
+		shooting = false
