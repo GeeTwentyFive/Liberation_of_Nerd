1cd924bc8121af5fb63a6a38a58c1f4b183a2259
Gee <alickoamarko@gmail.com>
2025-06-29 17:32:18 +0200

    ADDED PORTAL (SUPER SCUFFED)

---
+++ Portal/portal.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+
+# MAKE SURE PARENT IS GameManager
+
+
+const MAP = preload("res://Map/Map.tscn")
+
+
+var ran := false
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if ran: return
+		ran = true
+		collision_mask = 0
+		body.reparent(self)
+		body.held_weapon.reparent(self)
+		if body.held_item:
+			body.held_item.reparent(self)
+		var old_map = get_parent().get_node("Map")
+		while old_map:
+			old_map.queue_free()
+			await get_tree().process_frame
+		get_parent().add_child(MAP.instantiate())
+		var new_player = get_parent().get_node("Map").get_node("Player")
+		while new_player:
+			new_player.queue_free()
+			await get_tree().process_frame
+		var new_DDD = get_parent().get_node("Map").get_node("DDD")
+		while new_DDD:
+			new_DDD.queue_free()
+			await get_tree().process_frame
+		body.reparent(get_parent().get_node("Map"))
+		body.held_weapon.reparent(get_parent().get_node("Map"))
+		if body.held_item:
+			body.held_item.reparent(get_parent().get_node("Map"))
+		body.global_position = Vector2.ZERO
+		await get_tree().physics_frame
+		get_parent()._ready()
+		queue_free()
--- game_manager.gd
+++ game_manager.gd
@@ -1,9 +1,6 @@
 extends Node
 
 
-@onready var player: Player = $Map/Player
-
-
 func _input(event: InputEvent) -> void:
 	for action in InputMap.get_actions():
 		if event.is_action_pressed(action):
@@ -15,10 +12,12 @@ func _input(event: InputEvent) -> void:
 func _init() -> void:
 	Engine.max_fps = DisplayServer.screen_get_refresh_rate() * 2
 
-
 func _ready() -> void:
+	var player: Player = $Map/Player
+	
 	player.hit.connect(get_tree().reload_current_scene)
 	
 	for node in $Map.get_children():
 		if node is Enemy:
+			node.process_mode = Node.PROCESS_MODE_DISABLED
 			node.target = player
